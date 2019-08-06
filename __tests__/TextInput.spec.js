ca5b07f0f7865be90fd78a8a552be199a1415d8d
Prateek Kr. Singh <prateek@mobileoffize.com>
2019-08-06 16:36:59 +0530

    Prettify js files

--- __tests__/TextInput.spec.js
+++ __tests__/TextInput.spec.js
@@ -1,31 +1,30 @@
-import React from "react";
+import React from 'react';
 import { shallow, mount } from 'enzyme';
-import FormO from "../src/index";
-import TextInputField from "../src/fields/textInput/index";
+import FormO from '../src/index';
+import TextInputField from '../src/fields/textInput/index';
 
 const testSchema = {
-  "type": "text",
-  "name": "data_source",
-  "label": "Data Source",
-  "required": true,
-  "editable": true,
-  "hidden": false
+    type: 'text',
+    name: 'data_source',
+    label: 'Data Source',
+    required: true,
+    editable: true,
+    hidden: false,
 };
 
-const fields= require("../schema/00-basic_types.json");
+const fields = require('../schema/00-basic_types.json');
 // const fields1=require("../example/sampleform/src/schema/01-phone-and-email.json");
 
-describe(FormO,()=>{
-  it('Renders FormO',()=>{
-    const form = mount(<FormO fields={fields}/>);
-    expect(FormO).toMatchSnapshot();
-  })
+describe(FormO, () => {
+    it('Renders FormO', () => {
+        const form = mount(<FormO fields={fields} />);
+        expect(FormO).toMatchSnapshot();
+    });
 });
 
-describe(TextInputField,()=>{
-  it('Renders text Input field',()=>{
-    const inputField=shallow(<TextInputField attributes={testSchema} />);
-    expect(TextInputField).toMatchSnapshot();
-  })
-})
-
+describe(TextInputField, () => {
+    it('Renders text Input field', () => {
+        const inputField = shallow(<TextInputField attributes={testSchema} />);
+        expect(TextInputField).toMatchSnapshot();
+    });
+});
