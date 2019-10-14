35255770fecbcfee4316cb0906e2a13862bae110
Krupanand <krupanand@mobileoffize.com>
2019-10-14 11:30:26 +0530

    Add modal for lookup

--- packages/react-web-formo/src/fields/lookup/index.js
+++ packages/react-web-formo/src/fields/lookup/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
 import { isEmpty } from './../../utils/validators';
 import './style.css';
 
@@ -6,15 +7,23 @@ export default class Lookup extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      modalVisible: false,
-      value: null,
+      openLookupOptions: false,
+      attributes: this.props.attributes,
+      modalIsOpen: false,
     };
   }
 
-  handleChange = event => {
-    let val = event.target.value;
-    this.setState({ value: val });
-    this.props.updateValue(this.props.attributes.name, val);
+  handleChange = data => {
+    this.props.updateValue(this.props.attributes.name, data);
+    this.closeModal();
+  };
+
+  openModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
   };
 
   getLabel = () => {
@@ -43,10 +52,12 @@ export default class Lookup extends Component {
   renderLookupData = () => {
     const { attributes } = this.props;
     return (
-      <div className="lookup-data-wrapper">
+      <div className="lookup-data-wrapper" onClick={this.openModal}>
         <p style={{ paddingStart: 5 }}>{attributes.label}</p>
-        {this.getLabel()}
-        <i className="fal fa-angle-right"></i>
+        <div className="value-icon-wrapper">
+          <h6>{this.getLabel()}</h6>
+          <i className="fal fa-angle-right"></i>
+        </div>
       </div>
     );
   };
@@ -54,25 +65,89 @@ export default class Lookup extends Component {
   renderOptions = () => {
     const { attributes } = this.props;
     return (
-      <div className="lookup-options-wrapper">
+      <Modal
+        isOpen={this.state.modalIsOpen}
+        onRequestClose={this.closeModal}
+        className="modal"
+        overlayClassName="overlay"
+        ariaHideApp={false}
+      >
         {attributes.options.length > 0 &&
           attributes.options.map((item, index) => {
             return (
-              <div key={index} className="lookup-options-value">
+              <div
+                key={index}
+                className="lookup-options-value"
+                onClick={() => this.handleChange(item)}
+              >
                 {item[attributes.labelKey]}
               </div>
             );
           })}
-      </div>
+      </Modal>
     );
   };
 
   render() {
     const { attributes } = this.props;
     return (
-      <div className="lookup-wrapper">
-        {this.renderLookupData()}
-        {this.renderOptions()}
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          margin: 10,
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginBottom: 8,
+          }}
+        >
+          <div
+            style={{
+              width: '30%',
+              display: 'flex',
+              justifyContent: 'flex-start',
+            }}
+          >
+            {attributes['required'] && (
+              <p style={{ fontSize: 16, margin: 0 }}>
+                {attributes['label']} * :
+              </p>
+            )}
+            {!attributes['required'] && (
+              <p style={{ fontSize: 16, margin: 0 }}>{attributes['label']}:</p>
+            )}
+          </div>
+          <div
+            style={{
+              width: '70%',
+              display: 'flex',
+              justifyContent: 'flex-end',
+            }}
+          >
+            {attributes['error'] && (
+              <p
+                id="error"
+                style={{
+                  color: 'red',
+                  fontSize: 12,
+                  margin: 0,
+                }}
+              >
+                {attributes['errorMsg']}
+              </p>
+            )}
+          </div>
+        </div>
+        <div className="lookup-wrapper">
+          {this.renderLookupData()}
+          {this.state.modalIsOpen ? this.renderOptions() : ''}
+        </div>
       </div>
     );
   }
