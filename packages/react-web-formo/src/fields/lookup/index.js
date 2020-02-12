ed5c265988ad4784c6b34ae751e8f47c1cff4aa9
Krupanand <krupanand@mobileoffize.com>
2020-02-12 17:50:56 +0530

    Removed lookup modal

--- packages/react-web-formo/src/fields/lookup/index.js
+++ packages/react-web-formo/src/fields/lookup/index.js
@@ -1,30 +1,18 @@
 import React, { Component } from 'react';
-import Modal from 'react-modal';
-import Pagination from './pagination';
 import { isEmpty } from './../../utils/validators';
 import './style.css';
 
 export default class Lookup extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      rowsPerPage: 50,
-      page: 0,
-      modalIsOpen: false,
-    };
+    this.state = {};
   }
 
-  handleChange = data => {
-    this.props.updateValue(this.props.attributes.name, data);
-    this.closeModal();
-  };
-
-  openModal = () => {
-    this.setState({ modalIsOpen: true });
-  };
-
-  closeModal = () => {
-    this.setState({ modalIsOpen: false });
+  handleOnclick = () => {
+    if (typeof this.props.onClickLookup === 'function') {
+      this.props.onClickLookup(this.props);
+    }
+    return;
   };
 
   getSelectedValue = () => {
@@ -57,7 +45,7 @@ export default class Lookup extends Component {
     return (
       <div
         className={`lookup-data-wrapper ${disableCondition ? 'disabled' : ''}`}
-        onClick={this.openModal}
+        onClick={() => this.handleOnclick()}
       >
         <p style={{ paddingStart: 5 }}>{attributes.label}</p>
         <div className="value-icon-wrapper">
@@ -65,128 +53,6 @@ export default class Lookup extends Component {
           <i className="fal fa-angle-right"></i>
         </div>
       </div>
-    );
-  };
-
-  handleChangeRowsPerPage = data => {
-    this.setState({ rowsPerPage: Number(data) });
-  };
-
-  paginationStartNumber = page => {
-    const { attributes } = this.props;
-    if (attributes.total_records == 0) {
-      return 0;
-    } else if (page === 0) {
-      return 1;
-    } else {
-      return page * this.state.rowsPerPage + 1;
-    }
-  };
-
-  endRecordNumber = page => {
-    const { attributes } = this.props;
-    const endNumber = page * this.state.rowsPerPage + this.state.rowsPerPage;
-    if (attributes.total_records < endNumber) return attributes.total_records;
-    else return endNumber;
-  };
-
-  paginationEndNumber = page => {
-    const { attributes } = this.props;
-    if (attributes.total_records === 0) {
-      return 0;
-    } else if (page === 0) {
-      return attributes.options.length;
-    } else {
-      return this.endRecordNumber(page);
-    }
-  };
-
-  handleChangePage = text => {
-    const { attributes } = this.props;
-    if (text === 'dec') {
-      if (this.state.page !== 0) {
-        if (
-          attributes.data_source &&
-          attributes.data_source.referenceId.trim() !== ''
-        ) {
-          this.props.getLookupData(
-            attributes.data_source.referenceId,
-            this.paginationStartNumber(this.state.page - 1) - 1,
-            this.paginationEndNumber(this.state.page - 1)
-          );
-        }
-        this.setState({ page: this.state.page - 1 });
-      }
-    } else if (text === 'inc') {
-      if (attributes.total_records !== attributes.options.length) {
-        if (
-          attributes.data_source &&
-          attributes.data_source.referenceId.trim() !== ''
-        ) {
-          this.props.getLookupData(
-            attributes.data_source.referenceId,
-            this.paginationStartNumber(this.state.page + 1) - 1,
-            this.paginationEndNumber(this.state.page + 1)
-          );
-        }
-        this.setState({ page: this.state.page + 1 });
-      }
-    }
-    const element = document.getElementById('modal-title-wrapper');
-    element.scrollIntoView(true);
-  };
-
-  renderOptionsModal = () => {
-    const { attributes } = this.props;
-    return (
-      <Modal
-        isOpen={this.state.modalIsOpen}
-        onRequestClose={this.closeModal}
-        className="modal"
-        overlayClassName="overlay"
-        ariaHideApp={false}
-      >
-        <div id="modal-title-wrapper" style={{ marginBottom: '20px' }}>
-          <p>{`${attributes['label']} data`}</p>
-        </div>
-        <div className="modal-content-wrapper ">
-          {/* <div className="search-box-wrapper">
-            <div className="search-box">
-              <input
-                className="search-text"
-                type="text"
-                placeholder="Search..."
-                onChange={this.filterTeams}
-              />
-              <div className="search-btn">
-                <i className="fas fa-search" />
-              </div>
-            </div>
-          </div> */}
-          {attributes.options.length > 0 &&
-            attributes.options.map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  className="lookup-options-value"
-                  onClick={() => this.handleChange(item)}
-                >
-                  {item[attributes.labelKey]}
-                </div>
-              );
-            })}
-          <Pagination
-            rowsPerPage={this.state.rowsPerPage}
-            totalRecords={attributes.total_records}
-            page={this.state.page}
-            handleChangePage={this.handleChangePage}
-            handleChangeRowsPerPage={this.handleChangeRowsPerPage}
-            paginationStartNumber={this.paginationStartNumber}
-            endRecordNumber={this.endRecordNumber}
-            paginationEndNumber={this.paginationEndNumber}
-          />
-        </div>
-      </Modal>
     );
   };
 
@@ -204,10 +70,7 @@ export default class Lookup extends Component {
             {attributes['error'] && <p id="error">{attributes['errorMsg']}</p>}
           </div>
         </div>
-        <div className="lookup-content-wrapper">
-          {this.renderLookupUI()}
-          {this.state.modalIsOpen ? this.renderOptionsModal() : ''}
-        </div>
+        <div className="lookup-content-wrapper">{this.renderLookupUI()}</div>
       </div>
     );
   }
