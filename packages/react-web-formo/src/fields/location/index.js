fb7d6d7f55e741e747e010112e18df820c7b13c1
Krupanand <krupanand@mobileoffize.com>
2020-04-02 13:14:58 +0530

    Add location data type

---
+++ packages/react-web-formo/src/fields/location/index.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+
+export default class Location extends Component {
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          this.props.updateValue(
+            this.props.attributes.name,
+            `http://maps.google.com/maps?q=${position.coords.latitude},${position.coords.longitude}`
+          );
+        },
+        error => {
+          this.props.updateValue(
+            this.props.attributes.name,
+            `Unable to fetch location`
+          );
+        }
+      );
+    }
+  }
+
+  render() {
+    const { attributes } = this.props;
+    const disableCondition =
+      this.props.formSubmissionType === 'update' && !attributes.editable;
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          margin: 10,
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginBottom: 8,
+          }}
+        >
+          <p style={{ fontSize: 16, margin: 0 }}>
+            {attributes['label']} {attributes['required'] ? `*` : ''} :
+          </p>
+          {attributes['error'] && (
+            <p
+              id="error"
+              style={{
+                color: 'red',
+                fontSize: 12,
+                margin: 0,
+              }}
+            >
+              {attributes['errorMsg']}
+            </p>
+          )}
+        </div>
+        <div style={{ display: 'flex', height: 45 }}>
+          <input
+            type={attributes['type']}
+            value={attributes['value']}
+            id={attributes['name']}
+            disabled={disableCondition}
+            style={{
+              width: '100%',
+              border: '1px solid #979797',
+              borderRadius: 5,
+              padding: 5,
+              fontSize: 20,
+              outline: 'none',
+              opacity: disableCondition ? 0.5 : 1,
+            }}
+            onChange={() => console.log(attributes['value'])}
+          />
+        </div>
+      </div>
+    );
+  }
+}
