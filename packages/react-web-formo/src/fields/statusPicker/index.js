ce35c964baf06d33211fd2aee9bbc245f4753155
Shazia <shaziya@mobileoffize.com>
2025-09-08 18:33:49 +0530

    Add a arrow for status picker

--- packages/react-web-formo/src/fields/statusPicker/index.js
+++ packages/react-web-formo/src/fields/statusPicker/index.js
@@ -6,16 +6,51 @@ export default class StatusPickerField extends Component {
   };
 
   renderWebPicker = () => {
-    const { attributes } = this.props;
-    const disableCondition = !attributes.editable;
-    return (
+  const { attributes } = this.props;
+  const disableCondition = !attributes.editable;
+
+  const wrapStyle = {
+    position: 'relative',
+    width: '100%',
+  };
+  const selectStyle = {
+    width: '100%',
+    height: 45,
+    fontSize: 16,
+    borderRadius: '5px',
+    appearance: 'none',
+    WebkitAppearance: 'none',
+    MozAppearance: 'none',
+    color: attributes.show_first_option ? 'transparent' : 'inherit',
+    paddingRight: 36,
+    backgroundImage:
+      `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 24 24' fill='none' stroke='%23222222' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><polyline points='6 9 12 15 18 9'/></svg>")`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'right 12px center',
+    backgroundSize: '12px',
+  };
+
+  const valueOverlayStyle = {
+    position: 'absolute',
+    left: 8,
+    top: '50%',
+    transform: 'translateY(-50%)',
+    background: 'yellow',
+    borderRadius: 4,
+    padding: '2px 6px',
+    fontSize: 16,
+    lineHeight: '20px',
+    whiteSpace: 'nowrap',
+    maxWidth: 'calc(100% - 48px)',
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    pointerEvents: 'none',
+  };
+
+  return (
+    <div style={wrapStyle}>
       <select
-        style={{
-          width: '100%',
-          height: 45,
-          fontSize: 16,
-          borderRadius: '5px',
-        }}
+        style={selectStyle}
         value={attributes.value}
         onChange={this.handleChange}
         disabled={disableCondition}
@@ -26,8 +61,13 @@ export default class StatusPickerField extends Component {
           </option>
         ))}
       </select>
-    );
-  };
+
+      {attributes.show_first_option && attributes.value ? (
+        <span style={valueOverlayStyle}>{attributes.value}</span>
+      ) : null}
+    </div>
+  );
+};
 
   render() {
     const { attributes } = this.props;
