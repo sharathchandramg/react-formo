96509acaf0b00f654a60c1278025ef0087fc25f9
Shazia <shaziya@mobileoffize.com>
2025-09-09 11:49:03 +0530

    Add style in style.css

--- packages/react-web-formo/src/fields/statusPicker/index.js
+++ packages/react-web-formo/src/fields/statusPicker/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './style.css';
 
 export default class StatusPickerField extends Component {
   handleChange = (event) => {
@@ -9,47 +10,12 @@ export default class StatusPickerField extends Component {
     const { attributes } = this.props;
     const disableCondition = !attributes.editable;
 
-    const wrapStyle = {
-      position: 'relative',
-      width: '100%',
-    };
-    const selectStyle = {
-      width: '100%',
-      height: 45,
-      fontSize: 16,
-      borderRadius: '5px',
-      appearance: 'none',
-      WebkitAppearance: 'none',
-      MozAppearance: 'none',
-      color: attributes.show_first_option ? 'transparent' : 'inherit',
-      paddingRight: 36,
-      backgroundImage: `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 24 24' fill='none' stroke='%23222222' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><polyline points='6 9 12 15 18 9'/></svg>")`,
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'right 12px center',
-      backgroundSize: '12px',
-    };
-
-    const valueOverlayStyle = {
-      position: 'absolute',
-      left: 8,
-      top: '50%',
-      transform: 'translateY(-50%)',
-      background: 'yellow',
-      borderRadius: 4,
-      padding: '2px 6px',
-      fontSize: 16,
-      lineHeight: '20px',
-      whiteSpace: 'nowrap',
-      maxWidth: 'calc(100% - 48px)',
-      overflow: 'hidden',
-      textOverflow: 'ellipsis',
-      pointerEvents: 'none',
-    };
-
     return (
-      <div style={wrapStyle}>
+      <div className="status-picker-wrap">
         <select
-          style={selectStyle}
+          className={`status-picker-select ${
+            attributes.show_first_option ? 'status-picker-transparent' : ''
+          }`}
           value={attributes.value}
           onChange={this.handleChange}
           disabled={disableCondition}
@@ -62,7 +28,7 @@ export default class StatusPickerField extends Component {
         </select>
 
         {attributes.show_first_option && attributes.value ? (
-          <span style={valueOverlayStyle}>{attributes.value}</span>
+          <span className="status-picker-overlay">{attributes.value}</span>
         ) : null}
       </div>
     );
@@ -71,13 +37,7 @@ export default class StatusPickerField extends Component {
   render() {
     const { attributes } = this.props;
     return (
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          margin: 10,
-        }}
-      >
+      <div style={{ display: 'flex', flexDirection: 'column', margin: 10 }}>
         <div
           style={{
             display: 'flex',
@@ -91,14 +51,7 @@ export default class StatusPickerField extends Component {
             {attributes['label']} {attributes['required'] ? `*` : ''} :
           </p>
           {attributes['error'] && (
-            <p
-              id="error"
-              style={{
-                color: 'red',
-                fontSize: 12,
-                margin: 0,
-              }}
-            >
+            <p id="error" className="status-picker-error">
               {attributes['errorMsg']}
             </p>
           )}
